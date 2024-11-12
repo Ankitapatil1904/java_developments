29e0408b974d13561af91175249785b53a3c7ebe
Ankitapatil1904 <148199360+Ankitapatil1904@users.noreply.github.com>
2024-11-12 08:19:09 +0530

    Create Mutlithread.java

---
+++ Mutlithread.java
@@ -0,0 +1,14 @@
+package swing;
+
+public class Mutlithread {
+
+	public static void main(String[] args) {
+		Thread obj=	Thread.currentThread();
+		System.out.println("current thread is:"+obj);
+		System.out.println("thank-you");
+
+	}
+
+}
+
+
